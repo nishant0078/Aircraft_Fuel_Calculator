89f7bcdd6b1fc35675743a53785085a2692c08a0
nishant0078 <nishant426@hotmail.com>
2021-06-03 07:20:32 +0530

    Code Change Added Classes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@ int main()
 {  
     std::cout<< "Hello" ;
     
+    
 }
     
+class airplane 
+{
+    float weight;
+    float distance;
+    float fuel;
+    
+    public:
+    void data(float weight, float distance );
 
+    void putdata(void)
+    {
+        std::cout<< 
+    }
+    
+}
